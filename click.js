10848fde4edf04396e340b04b5baabcfebb47432
1921101 <87455022+1921101@users.noreply.github.com>
2021-07-24 23:52:17 +0900

    最終課題
    最終課題

---
+++ click.js
@@ -0,0 +1,15 @@
+function frameClick() {
+      document.location.href = "page1.html";
+}
+function frameClick2() {
+      document.location.href = "page2.html";
+}
+
+function frameClick3() {
+    document.location.href = "page3.html";
+}
+
+function frameClick4() {
+    document.location.href = "page4.html";
+}
+
